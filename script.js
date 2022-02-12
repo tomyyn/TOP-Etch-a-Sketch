53a18b1519f8b097e26daf71911db2b57f2fb5c9
Tom <tomasnaiouf@gmail.com>
2022-02-11 21:16:20 -0300

    Add grid Toggle

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function colorSquare(e){
 
 function createSquare(tam){
     const sqr=document.createElement("div");
-    sqr.style.cssText=`width: ${tam}px; height: ${tam}px; margin:0px; box-sizing: border-box; border: 1px solid black; background:white`;
+    sqr.style.cssText=`width: ${tam}px; height: ${tam}px; margin:0px; box-sizing: border-box; border: ${gridOn?"1px solid black":"0px"}; background:white`;
     sqr.addEventListener("mouseenter",colorSquare);
     return sqr;
 }
@@ -22,7 +22,7 @@ function createGrid(n){
     for(let i=0;i<n;i++) grid.appendChild(createSquare(tam));
 }
 
-function resize(e){
+function resize(){
     let n = prompt("Enter new size (in squares by side) between 1 and 100");
     while((n) && (n <1 || n>100 || isNaN(+n))){
         console.log(n);
@@ -31,7 +31,28 @@ function resize(e){
     if(n) createGrid(n);
 }
 
+function toggleGrid(e){
+    gridOn=!gridOn;
+    
+    sqrs=document.querySelectorAll("#grid div")
+    if(gridOn){
+        sqrs.forEach(sqr => {
+            sqr.style.border="1px solid black";
+        });
+        e.target.textContent="Grid: on"
+    }
+    else{
+        sqrs.forEach(sqr => {
+            sqr.style.border="0px";
+        });
+        e.target.textContent="Grid: off"
+    }
+}
+
 const resizeB=document.querySelector("#resize");
 resizeB.addEventListener("click",resize);
+
+const gridB=document.querySelector("#gridB");
+gridB.addEventListener("click",toggleGrid);
 
 createGrid(16);
